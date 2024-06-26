b73732b2085b20043a9239cfdd7ffcd19234529f
Siddharath Singh <siddharathsingh19@gmail.com>
2024-06-26 14:54:49 +0530

    Post Model

--- src/features/posts/post.controller.js
+++ src/features/posts/post.controller.js
@@ -1,8 +1,8 @@
 import { User } from "../users/user.model.js";
-import {Post} from "./post.schema.js";
+import { Post } from "./post.schema.js";
 import asyncHandler from "../../utils/asyncHandler.js";
 import ApiError from "../../utils/ApiError.js";
-import ApiResponse from "../../utils/ApiError.js";
+import ApiResponse from "../../utils/ApiResponse.js";
 
 export const getAllPosts = asyncHandler(async (req, res) => {
   const allPosts = await Post.find();
@@ -10,47 +10,84 @@ export const getAllPosts = asyncHandler(async (req, res) => {
     throw new ApiError(404, "Error in getting all posts.");
   }
   if (allPosts.length > 0) {
-    res.status(200).json(new ApiResponse(200, allPosts, ));
+    res.status(200).json(new ApiResponse(200, allPosts));
   } else {
     res.status(404).send("No posts found");
   }
 });
 
-export const getOnePost = asyncHandler(async(req, res)=>{
+export const getOnePost = asyncHandler(async (req, res) => {
   const postId = req.params.postId;
   const post = await Post.findById(postId);
   if (!post) {
-    throw new ApiError(404, "Error in finding post.")
+    throw new ApiError(404, "Error in finding post.");
   }
-  res.status(200).json(new ApiResponse(200, post, "Post found."))
-})
+  res.status(200).json(new ApiResponse(200, post, "Post found."));
+});
 
 export const createPost = asyncHandler(async (req, res) => {
   const { caption } = req.body;
-  const userId = req.user._id
-  const imageUrl = req.file?.path; 
+  const userId = req.user._id;
+  const imageUrl = req.file?.path;
 
   // Log the request body and file for debugging
-  console.log('Request Body:', req.body);
-  console.log("User ID : ", userId)
-  console.log('Uploaded File:', req.file);
-    // Basic validation
-    if (!caption || !imageUrl || !userId) {
-        throw new ApiError(400, "ALl fields are required.")
-    }
+  console.log("Request Body:", req.body);
+  console.log("User ID : ", userId);
+  console.log("Uploaded File:", req.file);
+  // Basic validation
+  if (!caption || !imageUrl || !userId) {
+    throw new ApiError(400, "ALl fields are required.");
+  }
 
-    // Create a new post
-      const post = await Post.create({
-          userId,
-          imageUrl,
-          caption,
-      });
-      const newPost = await Post.findById(post._id)
-      if (!newPost) {
-        throw new ApiError(404, "Error in new Post")
-      }
-      res.status(201).json(new ApiResponse(201, newPost, "Post created"))
-      
-    
+  // Create a new post
+  const post = await Post.create({
+    userId,
+    imageUrl,
+    caption,
+  });
+  const newPost = await Post.findById(post._id);
+  if (!newPost) {
+    throw new ApiError(404, "Error in new Post");
+  }
+  res.status(201).json(new ApiResponse(201, newPost, "Post created"));
+});
+
+export const getPostByUserId = asyncHandler(async (req, res) => {
+  const userId = req.params.userId;
+  const posts = await Post.find({ userId });
+  if (!posts) {
+    throw new ApiError(404, "Error in finding posts by user ID.");
+  }
+  res.status(200).json(new ApiResponse(200, posts, "All posts by user."));
+});
+
+export const deletePost = asyncHandler(async (req, res) => {
+  const postId = req.params.postId;
+  const userId = req.user._id;
+  const post = await Post.findById(postId);
+  if (!userId) {
+    throw new ApiError(404, "Error in finding post.");
+  }
+  await post.delete();
+  res.status(200).json(new ApiResponse(200, post, "Post deleted"));
+});
+
+export const updatePost = asyncHandler(async (req, res) => {
+  const postId = req.params.postId;
+  const userId = req.user._id;
+  const post = await Post.findById(postId);
+
+  if (!post) {
+      throw new ApiError(404, "Post not found");
+  }
+  if (post.userId.toString() !== userId.toString()) {
+      throw new ApiError(403, "Unauthorized");
+  }
+
+  // Update the post
+  post.caption = req.body.caption;
+  post.imageUrl = req.body.imageUrl;
+  await post.save();
+  res.status(200).json(new ApiResponse(200, post, "Post updated."));
 });
 
--- src/features/posts/post.routes.js
+++ src/features/posts/post.routes.js
@@ -1,9 +1,24 @@
 import { Router } from "express";
-import { createPost, getAllPosts } from "./post.controller.js";
-import jwtVerify from '../../middlewares/jwtAuth.js'
-import {upload} from '../../middlewares/multer.middleware.js'
+import {
+  createPost,
+  deletePost,
+  getAllPosts,
+  getOnePost,
+  getPostByUserId,
+  updatePost,
+} from "./post.controller.js";
+import jwtVerify from "../../middlewares/jwtAuth.js";
+import { upload } from "../../middlewares/multer.middleware.js";
 
-export const postRouter = Router()
+export const postRouter = Router();
 
-postRouter.route('/all').get(getAllPosts)
-postRouter.route('/create').post(jwtVerify, upload.single('imageUrl'), createPost)
+postRouter.route("/all").get(getAllPosts);
+postRouter
+  .route("/create")
+  .post(jwtVerify, upload.single("imageUrl"), createPost);
+postRouter.route("/:postId").get(getOnePost);
+postRouter.route("/").get(getPostByUserId);
+postRouter.route("/delete").post(jwtVerify, deletePost);
+postRouter
+  .route("/update")
+  .post(jwtVerify, upload.single("imageUrl"), updatePost);
